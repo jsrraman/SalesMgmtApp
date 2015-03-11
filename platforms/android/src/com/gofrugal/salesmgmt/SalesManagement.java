eef181ffa2cd9a9389b8ba27936047aeef9f03b7
Rajaraman <rajaraman.ks@gmail.com>
2015-03-11 16:59:33 +0530

    Updated

--- platforms/android/src/com/gofrugal/salesmgmt/SalesManagement.java
+++ platforms/android/src/com/gofrugal/salesmgmt/SalesManagement.java
@@ -22,7 +22,7 @@ package com.gofrugal.salesmgmt;
 import android.os.Bundle;
 import org.apache.cordova.*;
 
-public class MainActivity extends CordovaActivity
+public class SalesManagement extends CordovaActivity
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
